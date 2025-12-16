91781152887b328f42b67a6beb13eb1094150118
Jack Wilson <jwilso39@alumni.nd.edu>
2025-12-15 18:10:31 -0600

    creating beer and brewery objects

--- beer.py
+++ beer.py
@@ -1,12 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
 """
 Docstring for beer
 
 Functions for looking up beer on Untappd (using scraping tools) given search term
 """
 class Beer:
+    def __init__(self, soup: BeautifulSoup) -> None:
+        #grab id from href tag
+        id_tag = soup.find('a', class_='label')
+        self.id = int(id_tag['href'].split("/")[-1])
 
-    def __init__(self, html) -> None:
-        pass
+        #grab name/url
+        name = soup.find('p', class_='name')
+        self.name = name.text
+        self.url = f"https://untappd.com{name.a['href']}"
+
+        #grab img url
+        self.img_url = soup.find('img')['src']      
+
+        #grab brewery
+        brewery_href = soup.find('p', class_='brewery').a['href']
+        brewery_url = f"https://untappd.com{brewery_href}"
+        brewery_soup = Brewery.find_brewery(brewery_url)
+        self.brewery = Brewery(brewery_soup)
+
+        #grab other details
+        self.style = soup.find('p', class_='style').text
+        self.abv = float(soup.find('p', class_='abv').text.strip().split("%")[0])
+        self.ibu = int(soup.find('p', class_='ibu').text.strip().split()[0])
+        self.rating = float(soup.find('div', class_='caps')['data-rating'])
 
     """
     Finds beer given search term by first opening the right serach page, then picking the first
@@ -17,16 +40,35 @@ class Beer:
     Returns: First result of search term's Untappd page html
     """
     @classmethod
-    def find_beer(cls, search):
-        pass
+    def find_beer(cls, search: str):
+        search = search.replace(" ","+")
+        #use User-Agent headers to get actual response
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        }
+        URL=f"https://untappd.com/search?q={search}&type=beer&sort=all"
+        response = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        #grab just the HTML of the first result
+        return soup.find('div', class_='results-container').find('div', class_='beer-item')
 
-    """
-    Given beer HTML page, determine if it matches a requirements of the grid
+class Brewery:
+    def __init__(self, soup: BeautifulSoup) -> None:
+        self.name = soup.find('div', class_='name').h1.text.strip()
+        loc = soup.find('p', class_='brewery').text.strip().split(",")
+        self.city = loc[0]
+        self.state = loc[1].split()[0]
+        self.style = soup.find('p', class_='style').text
+        ratings_str = soup.find('p', class_='raters').text.strip().split()[0]
+        self.ratings = int(ratings_str.replace(",",""))
+        self.site = soup.find('a', class_='url tip')['href']
 
-    Params: beer - Beer object given by find_beer
-            req - Requirement object to compare beer to.
 
-    Returns: True if the Beer Object passes the requirment, False if not
-    """
-    def compare_beer(self, req):
-        pass
+    @classmethod
+    def find_brewery(cls, url):
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        }
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup.find('div', class_='content')
--- requirement.py
+++ requirement.py
@@ -6,3 +6,13 @@ Describes a test for one of the grids columns or rows.
 class Requirement:
     def __init__(self) -> None:
         pass
+
+    """
+    Given beer HTML page, determine if it matches this requirement of the grid
+
+    Params: beer - Beer object to compare to
+
+    Returns: True if the Beer Object passes the requirment, False if not
+    """
+    def compare_beer(self, beer):
+        pass
