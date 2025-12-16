9ff32a74628e4e74af5fbb667a9b615dc38fbded
Jack Wilson <jwilso39@alumni.nd.edu>
2025-12-16 16:13:23 -0600

    html begin

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from flask import Flask, request, render_template, session
+
+app = Flask(__name__)
+app.secret_key = """obviously in a real situation this would be somewhere else 
+but worried about testers not being able to replicate if I do that
+"""
+
+@app.route("/") # type: ignore
 def main():
-    #Create Grid with random columns/rows
-    #Allow for input from user and loop through options
-    #Output score after nine attempts
-    pass
+    """
+    Main page to play game
+    """
+    return render_template('grid.html')
 
 if __name__ == "__main__":
     main()
